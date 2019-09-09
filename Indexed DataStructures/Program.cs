0348edbe06666ead3ad1370daf51d23242eb006a
Mojtaba Khooryani <m.khooryani@outlook.com>
2019-09-09 19:44:20 +0430

    Add project files.

---
+++ Indexed DataStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Indexed_DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
