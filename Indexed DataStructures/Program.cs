4dafd1d587ce64b4f85719528529d4f12014a562
Mojtaba Khooryani <m.khooryani@outlook.com>
2019-09-11 14:10:37 +0430

    IndexedSortedSet class added

--- Indexed DataStructures/Program.cs
+++ Indexed DataStructures/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Indexed_DataStructures
 {
@@ -6,6 +7,20 @@ namespace Indexed_DataStructures
     {
         static void Main(string[] args)
         {
+            //SortedSet<int> set = new SortedSet<int>();
+            IndexedSortedSet<int> set = new IndexedSortedSet<int>();
+            set.Add(1);
+            set.Add(2);
+            set.Add(10);
+            set.Add(5);
+            var ss = set.GetEnumerator();
+            var sss = ss.Current;
+            ss.MoveNext();
+            sss = ss.Current;
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
             Console.WriteLine("Hello World!");
         }
     }
