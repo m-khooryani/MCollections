63f0e182d0d43cac511cc120e0e7c5c7a13f6345
Mojtaba Khooryani <m.khooryani@outlook.com>
2019-09-11 14:45:47 +0430

    separate files

---
+++ Indexed DataStructures/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace Indexed_DataStructures
+{
+    internal sealed class Node<T>
+    {
+        public T Item { get; private set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T item)
+        {
+            this.Item = item;
+        }
+    }
+}
---
+++ Indexed DataStructures/Tree.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace Indexed_DataStructures
+{
+    internal sealed class Tree<T>
+    {
+        Node<T> root;
+        private IComparer<T> comparer;
+        private int count;
+
+        public Tree(IComparer<T> comparer)
+        {
+            this.comparer = comparer;
+        }
+
+        public bool AddIfNotPresent(T item)
+        {
+            if (this.root == null)
+            {
+                this.root = new Node<T>(item);
+                this.count = 1;
+                return true;
+            }
+            Node<T> node = this.root;
+            Node<T> parent = this.root;
+            int x = 0;
+            while (node != null)
+            {
+                x = this.comparer.Compare(item, node.Item);
+                if (x == 0)
+                {
+                    return false;
+                }
+                var temp = node;
+                node = x > 0 ? parent.Right : parent.Left;
+                parent = temp;
+            }
+            node = new Node<T>(item);
+            if (x > 0)
+            {
+                parent.Right = node;
+            }
+            else
+            {
+                parent.Left = node;
+            }
+            this.count++;
+            return true;
+        }
+
+        public IEnumerator<T> DFS()
+        {
+            List<T> list = new List<T>();
+            this.DFS(this.root, list);
+            return list.GetEnumerator();
+        }
+
+        private void DFS(Node<T> node, List<T> list)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            DFS(node.Left, list);
+            list.Add(node.Item);
+            DFS(node.Right, list);
+        }
+    }
+}
